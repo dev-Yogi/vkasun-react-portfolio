461ca55c3ef1809d070bbce02b1249330b3da89c
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-04-05 23:22:36 -0500

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const compression = require('compression');
-const path = require('path');
-const app = express();
+{
+    /*const express = require('express');
+    const compression = require('compression');
+    const path = require('path');
+    const app = express();
 
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+    app.use(compression());
+    app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 
-const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`App is running on port ${PORT}`);
+    });
+    */
+}
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
