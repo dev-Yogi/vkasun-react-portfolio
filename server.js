56c9402efa05657814551616bc6eb2aa7bc657ec
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-04-05 23:28:47 -0500

    jesus take the wheel

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-{
-    /*const express = require('express');
-    const compression = require('compression');
-    const path = require('path');
-    const app = express();
-
-    app.use(compression());
-    app.use(express.static(path.join(__dirname, 'build')));
-
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-
-    const PORT = process.env.PORT || 3000;
-
-    app.listen(PORT, () => {
-        console.log(`App is running on port ${PORT}`);
-    });
-    */
-}
-
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
