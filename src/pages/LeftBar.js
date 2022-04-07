44ec0df281dc5366ffe0708f1d1a1d03002cff22
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-04-07 13:54:51 -0500

    remove kaggle logo const

--- src/pages/LeftBar.js
+++ src/pages/LeftBar.js
@@ -3,7 +3,7 @@ import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 const GitLogo = require("../assets/logos/GitLogo.png");
 const LinkedinLogo = require("../assets/logos/LinkedinLogo.png");
-const KaggleLogo = require("../assets/logos/KaggleLogo.png");
+
 const MailLogo = require("../assets/logos/MailLogo.png");
 
 const JSIcon = require("../assets/icons/JSIcon.png");
@@ -98,7 +98,6 @@ const LeftBar = () => {
                 className="h-10 w-10 text-yellow_vs hover:cursor-pointer hover:h-12 hover:w-12"
               />
             </a>
-       
             <a href="https://www.linkedin.com/in/vanessa-kasun-15393a139/">
               <img
                 src={LinkedinLogo}
