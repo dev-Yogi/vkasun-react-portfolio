2a7a452641c2dfcb783813cd442be54b8b50eec4
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-04-07 12:32:34 -0500

    config server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
