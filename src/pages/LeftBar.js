f97e4ef9b15859a2481329b97de362546b694863
Vanessa <57110017+dev-Yogi@users.noreply.github.com>
2022-07-20 17:08:30 -0500

    update: add https://

--- src/pages/LeftBar.js
+++ src/pages/LeftBar.js
@@ -33,7 +33,7 @@ const LeftBar = () => {
             alt="TS Icon"
             className="w-7 mr-1  ml-5 text-yellow_vs"
           />
-          <p><a href="citycoins.netlify.app">Second Project</a></p>
+          <p><a href="https://citycoins.netlify.app/">Second Project</a></p>
         </div>
       </a>
       <a href="/">
